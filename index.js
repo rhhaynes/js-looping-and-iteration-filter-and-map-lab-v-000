01b201e43dc446562b9d3c69877b3211ef440c1f
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-22 15:43:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function driversWithRevenueOver(arr, revenue){
   return arr.filter(
-    function(obj){return obj.revenue > revenue;}
+    function (obj){return obj.revenue > revenue;}
   );
 }
 
 function driverNamesWithRevenueOver(arr, revenue){
   return driversWithRevenueOver(arr, revenue).map(
-    function(obj){return obj.name;}
+    function (obj){return obj.name;}
   );
 }
 
